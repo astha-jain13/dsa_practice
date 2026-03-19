5edc864ccd436d3bc5cfd9d9081b558c894eaddc
astha-jain13 <asthajain.1309@gmail.com>
2026-03-19 15:59:51 +0530

    Add Drinks_200B_optimized.cpp solution
    Implement solution for the 'Drinks' problem on Codeforces.

---
+++ Arrays/Drinks_200B_optimized.cpp
@@ -0,0 +1,29 @@
+/*
+Problem: Drinks (Orange Juice)
+Platform: Codeforces
+Difficulty: Easy
+Concept: Average
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,juice;
+    float sum,orange_cocktail;
+    //Enter number of juices in fridge
+    cin>>n;
+    
+    //Enter fraction of orange juice in juices
+    for(int i=0;i<n;i++){
+        cin>>juice;
+        sum += juice;
+    }
+    
+    //Calculate percentage of orange juice in cocktail
+    orange_cocktail = sum/n;
+    
+    //Print output
+    cout<<orange_cocktail;
+}
