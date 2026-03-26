7c3e79f7b1b5aa991bbc6c4299507066227a6bcb
astha-jain13 <asthajain.1309@gmail.com>
2026-03-26 23:30:34 +0530

    Update comments for clarity in Vanya_and_Fence_677A.cpp

--- Arrays/Vanya_and_Fence_677A.cpp
+++ Arrays/Vanya_and_Fence_677A.cpp
@@ -14,20 +14,20 @@ int main()
 {
    int n,h,width=0;
    
-   //Enter the no. of friends
+   //Take input of number of friends
    cin>>n;
    
-   //Enter the height of fence
+   //Take input of height of fence
    cin>>h;
    
    int height[n];
    
-   //Enter heights of friends
+   //Take input of heights of friends
    for(int i=0;i<n;i++){
        cin>>height[i];
    }
    
-   //Comparison
+   //Compare the height of friends with fence
    for(int i=0;i<n;i++){
        if(height[i]>h){
            width+=2;
